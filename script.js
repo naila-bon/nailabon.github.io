375f7efb01aef1862c0dcd4db471b3bbaa35616d
naila-bon <naila.ambn@gmail.com>
2024-05-05 22:24:46 +0200

    Resolved merge conflicts

--- script.js
+++ script.js
@@ -5,82 +5,68 @@ import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/js
 // To allow for importing the .gltf file
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
-//Create a Three.JS Scene
+// Setup
 const scene = new THREE.Scene();
-//create a new camera with positions and angles
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-//Keep track of the mouse position, so we can make the eye move
-let mouseX = window.innerWidth / 2;
-let mouseY = window.innerHeight / 2;
+const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
 
-//Keep the 3D object on a global variable so we can access it later
-let object;
+//Add lights to the scene
+const ambientLight = new THREE.AmbientLight(0x333333);
+const pointLight = new THREE.PointLight(0xffffff, 1, 100);
+pointLight.position.set(5, 5, 5);
+scene.add(ambientLight, pointLight)
 
-//OrbitControls allow the camera to move around the scene
-let controls;
-
-//Set which object to render
-let objToRender = 'eye';
-
-//Instantiate a loader for the .gltf file
+//Add mesh
 const loader = new GLTFLoader();
 
-//Load the file
-loader.load(
-  `model.gltf`,
-  function (gltf) {
-    //If the file is loaded, add it to the scene
-    object = gltf.scene;
-    scene.add(object);
-  },
-  function (xhr) {
-    //While it is loading, log the progress
-    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-  },
-  function (error) {
-    //If there is an error, log it
-    console.error(error);
-  }
-);
+loader.load('model.gltf', (gltf) => {
+  const model = gltf.scene;
+  scene.add(model);
+});
 
-//Instantiate a new renderer and set its size
-const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
-renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.set(-5, 4, 5 );
+camera.rotation.y = -0.8
 
-//Add the renderer to the DOM
-document.getElementById("container3D").appendChild(renderer.domElement);
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-//Set how far the camera will be from the 3D model
-camera.position.z = objToRender === "dino" ? 25 : 500;
-
-//Add lights to the scene, so we can actually see the 3D model
-const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
-topLight.position.set(500, 500, 500) //top-left-ish
-topLight.castShadow = true;
-scene.add(topLight);
-
-const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
-scene.add(ambientLight);
-
-//This adds controls to the camera, so we can rotate / zoom it with the mouse
-if (objToRender === "dino") {
-  controls = new OrbitControls(camera, renderer.domElement);
-}
-
-//Render the scene
 function animate() {
-  requestAnimationFrame(animate);
-  //Here we could add some code to update the scene, adding some automatic movement
-
-  //Make the eye move
-  if (object && objToRender === "eye") {
-    //I've played with the constants here until it looked good 
-    object.rotation.y = -3 + mouseX / window.innerWidth * 3;
-    object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
-  }
-  renderer.render(scene, camera);
+  renderer.render(scene, camera)
+  requestAnimationFrame(animate)
 }
+
+animate()
+
+function moveCamera(){
+  const t = document.body.getBoundingClientRect().top;
+  const scrollPercentage = (t / (document.body.scrollHeight - window.innerHeight)) * 100;
+
+
+  let targetPosition;
+      let targetRotation;
+
+      if (scrollPercentage < 25) {
+        targetPosition = new THREE.Vector3(-5, 4, 5);
+        targetRotation = new THREE.Vector3(0, -0.8, 0);
+      } else if (scrollPercentage < 50) {
+        targetPosition = new THREE.Vector3(-5, 4, 0);
+        targetRotation = new THREE.Vector3(0, -1.6, 0);
+      } else if (scrollPercentage < 75) {
+        targetPosition = new THREE.Vector3(0, 4, 5);
+        targetRotation = new THREE.Vector3(0, 0, 0);
+      } else {
+        targetPosition = new THREE.Vector3(-5, 4, 5);
+        targetRotation = new THREE.Vector3(0, -0.8, 0);
+      }
+
+      camera.position.lerp(targetPosition, 0.1);
+      camera.rotation.y += (targetRotation.y - camera.rotation.y) * 0.05;
+    }
+
+// Add a scroll event listener to move the camera
+window.addEventListener("scroll", moveCamera);
 
 //Add a listener to the window, so we can resize the window and the camera
 window.addEventListener("resize", function () {
@@ -89,11 +75,10 @@ window.addEventListener("resize", function () {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-//add mouse position listener, so we can make the eye move
-document.onmousemove = (e) => {
-  mouseX = e.clientX;
-  mouseY = e.clientY;
-}
 
-//Start the 3D rendering
-animate();
+//Helpers
+// const lightHelper = new THREE.PointLightHelper(ambientLight)
+// const gripHelper = new THREE.GridHelper(10, 10, 0xaec6cf, 0xaec6cf)
+// const cameraHelper = new THREE.CameraHelper(camera);
+// scene.add(lightHelper, gripHelper, cameraHelper)
+
